1e58157631aeab0455dce5c2119c4a3d919fc958
Richard Barnes <rbarnes@umn.edu>
2013-04-28 12:00:52 -0500

    Initial import

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This program generates the figures in the manuscript
+#
+#"Modeling of bovine spongiform encephalopathy in a two-species feedback loop"
+#By: Richard Barnes and Clarence Lehman
+#doi: 10.1016/j.epidem.2013.04.001
+#
+#This code was written by Richard Barnes (rbarnes@umn.edu)
+
+#Show a single species loop with lifespan held at 5 years
+./single_species_model.py -k 10 -r 3 -n 5 -c 0.01 > fig_single5years.dat
+
+#Show a single species loop with lifespan held at 4 years
+./single_species_model.py -k 10 -r 3 -n 4 -c 0.01 > fig_single4years.dat
+
+#Show a two-species loop with lifespans held at 8 years and 3 years
+#Material is fed back to a single age class in both species
+./two_species_model.py -k 10 -r 3 -s 0.8 -n 8 -m 3 -c 0.02,0,0,0,0,0,0,0,0.02 -y 40 > fig_two_species.dat
+
+#Show a two-species loop with lifespans held at 8 years and 3 years
+#Material is fed back to multiple age classes in both species
+./two_species_model.py -k 10 -r 4 -s 0.7 -n 8 -m 3 -c 0.005,0.005,0.005,0,0,0,0,0,0.005,0.005,0 -y 40 > fig_two_species_multiple_feedback.dat
